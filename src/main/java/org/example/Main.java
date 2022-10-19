87ccd6c503947fcaa8e61540bbe29d5793bf679d
Mirko Petrovic <mirkic.kg99@gmail.com>
2022-10-19 15:40:10 +0200

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         File folder = new File("C:\\Users\\mirkop\\IdeaProjects\\anyWord");
         File[] listOfFiles = folder.listFiles();
+        
         ExecutorService service= Executors.newCachedThreadPool();
         for(int i=0;i<listOfFiles.length;i++){
             if(!listOfFiles[i].isFile()){continue;}
